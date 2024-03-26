8ea95888e9883f6d800bbea4f5692c8328b7d459
juliamistieri <julia.mistieri02@gmail.com>
2024-03-25 23:10:30 -0400

    parei na parte do node index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const conn = require("./db/conn");
+
+conn
+    .authenticate()
+    .then(()=>{
+    console.log("Conectado com sucesso!");
+    })
+    .catch((err)=>{
+    console.log("Ocorreu um erro: " + err);
+});

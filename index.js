a61ef8c422fd3fc8adef8ab2968d008e0ca740b0
juliamistieri <julia.mistieri02@gmail.com>
2024-04-08 21:56:36 -0400

    commit atualização final

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const conn = require("./db/conn");
 
 const Usuario = require("../Models/Usuario")
@@ -22,15 +23,18 @@ app.post("/usuarios/novo", async (req, res) => {
     const nickname = req.body.nickname;
     const nome = req.body.nome;
 
-    const dadosUsuario = {
-        nickname,
-        nome,
-    };
+    client.query(
+        `INSERT INTO usuarios (usuario_nickname, usuario_nome)
+        values ('${nick}', '${nome}') returning *`,
+        (err, result) => {
+            //callback
+        }
+    ) 
+    });
 
     const usuario = await Usuario.create(dadosUsuario);
 
     res.send("Usuário inserido sob o id: " + usuario.id);
-});
 
 app.get("/Jogo/novo", (req, res) => {
     res.sendFile('${__dirname}/views/formJogo.html');
@@ -67,20 +71,42 @@ conn
 const { Client } = require("pg");
 
 const client = new Client({
-    user: "postgress",
-    password: "Julia2002",
-    host: "localhost",
-    port: "5432",
-    database: "nome_do_banco",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
 });
 
 client
     .connect()
     .then(() => {
         console.log("Conectado ao Banco de Dados PostGreSQL");
+        //exibeUsuariosCadastrados
     })
     .catch((err) => {
         console.log('Erro: ${err}');
     });
 
-    
+    function exibeUsuariosCadastrados() {
+        cliente.query("select * from usuarios", (err, result) => {
+            if (err) {
+                console.error("Erro ao executar a busca: " + err);
+            }
+            else {
+                console.log("Resultado: " + JSON.stringify(result.rows));
+            }
+            fechaConexao();
+        });
+    }
+
+    function fechaConexao() {
+        client 
+        .end()
+        .then(() => {
+            console.log("Conexão encerrada!");
+        })
+        .catch((err) => {
+            console.error("Erro ao encerrar conexão: ", err);
+        });
+    }
